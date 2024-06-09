95bfa0fd56348ec38d5bfcfe2d8ebbec373cb6a2
kalpana-mind <152434265+kalpana-mind@users.noreply.github.com>
2024-06-08 20:05:11 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import SearchBar from "./components/SearchBar";
+import searchImages from "./api";
+import ImageList from "./components/ImageList";
+
+function App(){
+    const [images, setImages] = useState([]);
+    const handleSubmit = async (term) =>{
+       const results = await searchImages(term);
+       setImages(results);
+    };
+
+    return <div>
+        <SearchBar onSubmit={handleSubmit} />
+        <ImageList images={images}/>
+    </div>;
+}
+
+export default App;
